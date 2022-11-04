0e765dcad230c6890f7116b6c9f55a2040b43cd9
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-04 15:11:05 +0500

    refactor function getInfo

--- getInfo.js
+++ getInfo.js
@@ -4,42 +4,47 @@ import iconv from 'iconv-lite'
 import AdmZip from 'adm-zip'
 import xml2js from 'xml2js'
 
-const url = 'http://www.cbr.ru/s/newbik'
+const getInfo = async () => {
+  const url = 'http://www.cbr.ru/s/newbik'
 
-fetch(url)
-.then(res => {
-  res.body.pipe(fs.createWriteStream('bics.zip'))
-})
+  const response = await fetch(url)
 
-const zip = new AdmZip('./bics.zip');
-const newZip = new AdmZip();
+  if (!response.ok) throw new Error(`unexpected response ${response.statusText}`);
 
-const zipEntries = zip.getEntries();
-
-zipEntries.forEach(function(zipEntry) {
-  const fileName = zipEntry.entryName;
-  const fileContent = zip.readFile(fileName)
-  const newFileContent = iconv.decode(fileContent, 'win1251');
-  const newFileName = 'zip_content.xml'
-  newZip.addFile(newFileName, newFileContent, '')
-});
-
-newZip.extractAllTo('./', true)
-
-const str = newZip.readAsText('zip_content.xml')
-
-xml2js.parseString(str, (err, result) => {
-  if (err) {
-    throw err
-  }
-  const resultFile = result.ED807.BICDirectoryEntry.map((bic) => (
-    bic.Accounts?.map((account) => (
-      {
-        bic: bic.$.BIC,
-        name: bic.ParticipantInfo[0].$.NameP,
-        corrAccount: account.$.Account
+  response.body.pipe(fs.createWriteStream('bics.zip')).on("finish", () => {
+    const zip = new AdmZip('./bics.zip');
+    const newZip = new AdmZip();
+  
+    const zipEntries = zip.getEntries();
+  
+    zipEntries.forEach(function(zipEntry) {
+      const fileName = zipEntry.entryName;
+      const fileContent = zip.readFile(fileName)
+      const newFileContent = iconv.decode(fileContent, 'win1251');
+      const newFileName = 'zip_content.xml'
+      newZip.addFile(newFileName, newFileContent, '')
+    });
+  
+    newZip.extractAllTo('./', true)
+  
+    const str = newZip.readAsText('zip_content.xml')
+  
+    xml2js.parseString(str, (err, result) => {
+      if (err) {
+        throw err
       }
-    ))
-  )).flat(1).filter(Boolean)
-  fs.writeFileSync('result.txt', JSON.stringify(resultFile, null, 2) , 'utf-8');
-})
+      const resultArray = result.ED807.BICDirectoryEntry.map((bic) => (
+        bic.Accounts?.map((account) => (
+          {
+            bic: bic.$.BIC,
+            name: bic.ParticipantInfo[0].$.NameP,
+            corrAccount: account.$.Account
+          }
+        ))
+      )).flat(1).filter(Boolean)
+      fs.writeFileSync('result.txt', JSON.stringify(resultArray, null, 2) , 'utf-8');
+    })
+  })
+}
+
+export default getInfo
