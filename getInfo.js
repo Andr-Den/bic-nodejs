192347014e0ec03d0d8ed39f567c51c934f01e68
Andrey Denisov <86472463+Andr-Den@users.noreply.github.com>
2022-11-04 13:20:26 +0500

    create new zip file with new name

--- getInfo.js
+++ getInfo.js
@@ -5,3 +5,17 @@ fetch(url)
 .then(res => {
   res.body.pipe(fs.createWriteStream('bics.zip'))
 })
+
+import AdmZip from 'adm-zip'
+const zip = new AdmZip('./bics.zip');
+const newZip = new AdmZip();
+
+const zipEntries = zip.getEntries();
+
+zipEntries.forEach(function(zipEntry) {
+  const fileName = zipEntry.entryName;
+  const fileContent = zip.readFile(fileName)
+  const newFileContent = iconv.decode(fileContent, 'win1251');
+  const newFileName = 'zip_content.xml'
+  newZip.addFile(newFileName, newFileContent, '')
+});
